503cf0b5ea379f8ed066cbefd4c296127c5fbc65
ivanov1agka <ivanov1agka@mail.ru>
2022-03-25 22:00:38 +0300

    init commit

--- test/tests.cpp
+++ test/tests.cpp
@@ -2,3 +2,47 @@
 
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+using std::invalid_argument;
+using std::logic_error;
+
+TEST(test1, state_accept) {
+	Automata machine = Automata();
+	machine.on();
+	machine.coin(100);
+	States res = machine.getState();
+	EXPECT_EQ(Accept, res);
+}
+
+TEST(test2, index_1) {
+	Automata machine = Automata();
+	machine.on();
+	int res = machine.choise("Latte");
+	EXPECT_EQ(1, res);
+}
+
+TEST(test3, check_true) {
+	Automata machine = Automata();
+	machine.on();
+	machine.coin(100);
+	bool res = machin.check(1);
+	EXPECT_EQ(true, res);
+}
+
+TEST(test4, check_false) {
+	Automata machine = Automata();
+	machine.on();
+	machine.coin(10);
+	bool res = machin.check(1);
+	EXPECT_EQ(false, res);
+}
+
+TEST(test5, logic_error) {
+	Automata machine = Automata();
+	try {
+		machine.off();
+	}
+	catch(logic_error err){
+		ASSERT_STREQ("Error, invalid operation", err.what());
+	}
+}
