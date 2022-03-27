796c197a0e0548f872142d85e97caba586b4767b
Ivan Gulin <101045263+IGu1in@users.noreply.github.com>
2022-03-27 16:44:52 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,42 +7,42 @@ using std::invalid_argument;
 using std::logic_error;
 
 TEST(test1, state_accept) {
-	Automata machine = Automata();
-	machine.on();
-	machine.coin(100);
-	States res = machine.getState();
-	EXPECT_EQ(Accept, res);
+ Automata machine = Automata();
+ machine.on();
+ machine.coin(100);
+ States res = machine.getState();
+ EXPECT_EQ(Accept, res);
 }
 
 TEST(test2, index_1) {
-	Automata machine = Automata();
-	machine.on();
-	int res = machine.choise("Latte");
-	EXPECT_EQ(1, res);
+ Automata machine = Automata();
+ machine.on();
+ int res = machine.choice("Latte");
+ EXPECT_EQ(1, res);
 }
 
 TEST(test3, check_true) {
-	Automata machine = Automata();
-	machine.on();
-	machine.coin(100);
-	bool res = machin.check(1);
-	EXPECT_EQ(true, res);
+ Automata machine = Automata();
+ machine.on();
+ machine.coin(100);
+ bool res = machine.check(1);
+ EXPECT_EQ(true, res);
 }
 
 TEST(test4, check_false) {
-	Automata machine = Automata();
-	machine.on();
-	machine.coin(10);
-	bool res = machin.check(1);
-	EXPECT_EQ(false, res);
+ Automata machine = Automata();
+ machine.on();
+ machine.coin(10);
+ bool res = machine.check(1);
+ EXPECT_EQ(false, res);
 }
 
 TEST(test5, logic_error) {
-	Automata machine = Automata();
-	try {
-		machine.off();
-	}
-	catch(logic_error err){
-		ASSERT_STREQ("Error, invalid operation", err.what());
-	}
+ Automata machine = Automata();
+ try {
+  machine.off();
+ }
+ catch(logic_error err) {
+  ASSERT_STREQ("Error, invalid operation", err.what());
+ }
 }
